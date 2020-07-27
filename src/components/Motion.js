a03015ab75ac3bea26ba1ecaa70fb762f5027abb
Lincoln Anderson <landerson@threefivetwo.com>
2020-07-26 23:32:20 -0400

    add chakra motion components

---
+++ src/components/Motion.js
@@ -0,0 +1,186 @@
+import {
+  Accordion,
+  Alert,
+  AlertDialog,
+  Avatar,
+  AvatarBadge,
+  AspectRatioBox,
+  AvatarGroup,
+  Badge,
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  Button,
+  ButtonGroup,
+  Callout,
+  Code,
+  Checkbox,
+  CheckboxGroup,
+  CloseButton,
+  Collapse,
+  ControlBox,
+  CircularProgress,
+  CircularProgressLabel,
+  Divider,
+  Drawer,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Flex,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  FormErrorMessage,
+  Grid,
+  Heading,
+  Icon,
+  IconButton,
+  Image,
+  Input,
+  InputAddon,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  Kbd,
+  Link,
+  List,
+  ListItem,
+  ListIcon,
+  Stack,
+  SimpleGrid,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  MenuDivider,
+  MenuOptionGroup,
+  MenuItemOption,
+  Modal,
+  NumberInput,
+  Portal,
+  Popover,
+  Progress,
+  PseudoBox,
+  Radio,
+  RadioGroup,
+  RadioButtonGroup,
+  Select,
+  Skeleton,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  Spinner,
+  Stat,
+  Switch,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Tag,
+  TagIcon,
+  TagLabel,
+  TagCloseButton,
+  Text,
+  Textarea,
+  Tooltip,
+  VisuallyHidden,
+} from '@chakra-ui/core';
+import { motion } from 'framer-motion';
+
+const motorize = motion.custom;
+
+export const MotionAccordion = motorize(Accordion);
+export const MotionAlert = motorize(Alert);
+export const MotionAlertDialog = motorize(AlertDialog);
+export const MotionAvatar = motorize(Avatar);
+export const MotionAvatarBadge = motorize(AvatarBadge);
+export const MotionAspectRatioBox = motorize(AspectRatioBox);
+export const MotionAvatarGroup = motorize(AvatarGroup);
+export const MotionBadge = motorize(Badge);
+export const MotionBox = motorize(Box);
+export const MotionBreadcrumb = motorize(Breadcrumb);
+export const MotionBreadcrumbItem = motorize(BreadcrumbItem);
+export const MotionBreadcrumbLink = motorize(BreadcrumbLink);
+export const MotionBreadcrumbSeparator = motorize(BreadcrumbSeparator);
+export const MotionButton = motorize(Button);
+export const MotionButtonGroup = motorize(ButtonGroup);
+export const MotionCallout = motorize(Callout);
+export const MotionCode = motorize(Code);
+export const MotionCheckbox = motorize(Checkbox);
+export const MotionCheckboxGroup = motorize(CheckboxGroup);
+export const MotionCloseButton = motorize(CloseButton);
+export const MotionCollapse = motorize(Collapse);
+export const MotionControlBox = motorize(ControlBox);
+export const MotionCircularProgress = motorize(CircularProgress);
+export const MotionCircularProgressLabel = motorize(CircularProgressLabel);
+export const MotionDivider = motorize(Divider);
+export const MotionDrawer = motorize(Drawer);
+export const MotionEditable = motorize(Editable);
+export const MotionEditableInput = motorize(EditableInput);
+export const MotionEditablePreview = motorize(EditablePreview);
+export const MotionFlex = motorize(Flex);
+export const MotionFormControl = motorize(FormControl);
+export const MotionFormLabel = motorize(FormLabel);
+export const MotionFormHelperText = motorize(FormHelperText);
+export const MotionFormErrorMessage = motorize(FormErrorMessage);
+export const MotionGrid = motorize(Grid);
+export const MotionHeading = motorize(Heading);
+export const MotionIcon = motorize(Icon);
+export const MotionIconButton = motorize(IconButton);
+export const MotionImage = motorize(Image);
+export const MotionInput = motorize(Input);
+export const MotionInputAddon = motorize(InputAddon);
+export const MotionInputGroup = motorize(InputGroup);
+export const MotionInputLeftAddon = motorize(InputLeftAddon);
+export const MotionInputRightAddon = motorize(InputRightAddon);
+export const MotionKbd = motorize(Kbd);
+export const MotionLink = motorize(Link);
+export const MotionList = motorize(List);
+export const MotionListItem = motorize(ListItem);
+export const MotionListIcon = motorize(ListIcon);
+export const MotionStack = motorize(Stack);
+export const MotionSimpleGrid = motorize(SimpleGrid);
+export const MotionMenu = motorize(Menu);
+export const MotionMenuButton = motorize(MenuButton);
+export const MotionMenuList = motorize(MenuList);
+export const MotionMenuItem = motorize(MenuItem);
+export const MotionMenuGroup = motorize(MenuGroup);
+export const MotionMenuDivider = motorize(MenuDivider);
+export const MotionMenuOptionGroup = motorize(MenuOptionGroup);
+export const MotionMenuItemOption = motorize(MenuItemOption);
+export const MotionModal = motorize(Modal);
+export const MotionNumberInput = motorize(NumberInput);
+export const MotionPortal = motorize(Portal);
+export const MotionPopover = motorize(Popover);
+export const MotionProgress = motorize(Progress);
+export const MotionPseudoBox = motorize(PseudoBox);
+export const MotionRadio = motorize(Radio);
+export const MotionRadioGroup = motorize(RadioGroup);
+export const MotionRadioButtonGroup = motorize(RadioButtonGroup);
+export const MotionSelect = motorize(Select);
+export const MotionSkeleton = motorize(Skeleton);
+export const MotionSlider = motorize(Slider);
+export const MotionSliderTrack = motorize(SliderTrack);
+export const MotionSliderFilledTrack = motorize(SliderFilledTrack);
+export const MotionSliderThumb = motorize(SliderThumb);
+export const MotionSpinner = motorize(Spinner);
+export const MotionStat = motorize(Stat);
+export const MotionSwitch = motorize(Switch);
+export const MotionTabs = motorize(Tabs);
+export const MotionTabList = motorize(TabList);
+export const MotionTabPanels = motorize(TabPanels);
+export const MotionTab = motorize(Tab);
+export const MotionTabPanel = motorize(TabPanel);
+export const MotionTag = motorize(Tag);
+export const MotionTagIcon = motorize(TagIcon);
+export const MotionTagLabel = motorize(TagLabel);
+export const MotionTagCloseButton = motorize(TagCloseButton);
+export const MotionText = motorize(Text);
+export const MotionTextarea = motorize(Textarea);
+export const MotionTooltip = motorize(Tooltip);
+export const MotionVisuallyHidden = motorize(VisuallyHidden);
